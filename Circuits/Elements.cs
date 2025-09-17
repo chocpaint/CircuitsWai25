318bb26e8e23393ca9d5c29610d49143ad4f0342
Trey <tmn19@students.waikato.ac.nz>
2025-09-17 21:51:02 +1200

    moving more stuff around

--- Circuits/Elements.cs
+++ Circuits/Elements.cs
@@ -69,43 +69,6 @@ namespace Circuits
         }
 
 
-        /// <summary>
-        /// Checks if the gate has been clicked on.
-        /// </summary>
-        /// <param name="x">The x position of the mouse click</param>
-        /// <param name="y">The y position of the mouse click</param>
-        /// <returns>True if the mouse click position is inside the gate</returns>
-        public virtual bool IsMouseOn(int x, int y)
-        {
-            if (left <= x && x < left + WIDTH
-                && top <= y && y < top + HEIGHT)
-                return true;
-            else
-                return false;
-        }
-
-
-        /// <summary>
-        /// Moves the gate to the position specified.
-        /// </summary>
-        /// <param name="x">The x position to move the gate to</param>
-        /// <param name="y">The y position to move the gate to</param>
-        public virtual void MoveTo(int x, int y)
-        {
-            //Debugging message
-            Console.WriteLine("pins = " + pins.Count);
-            //Set the position of the gate to the values passed in
-            left = x;
-            top = y;
-
-            // more accurate drawing points for pins
-            pins[0].X = x - GAP;
-            pins[0].Y = y + HEIGHT / 2 - GAP;
-            pins[1].X = x - GAP;
-            pins[1].Y = y + HEIGHT / 2 + GAP;
-            pins[2].X = x + WIDTH + GAP;
-            pins[2].Y = y + HEIGHT / 2;
-        }
 
         /// <summary>
         /// abstract method used for drawing other gates
