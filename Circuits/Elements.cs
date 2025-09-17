31e3933e3534374f0266ecdc9ffde72ccb59a8fe
Trey <tmn19@students.waikato.ac.nz>
2025-09-17 22:04:05 +1200

    fixing elements and the vars relevant inside

--- Circuits/Elements.cs
+++ Circuits/Elements.cs
@@ -9,59 +9,27 @@ namespace Circuits
 {
     public abstract class Elements
     {
-
-        // left is the left-hand edge of the main part of the gate.
-        // So the input pins are further left than left.
-        protected int left;
-
-        // top is the top of the whole gate
-        protected int top;
-
-        /// <summary>
-        /// This is the list of all the pins of this gate.
-        /// An AND gate always has two input pins (0 and 1)
-        /// and one output pin (number 2).
-        /// </summary>
-        protected List<Pin> pins = new List<Pin>();
-        //Has the gate been selected
-        protected bool selected = false;
+        // vars
+        protected int left; // left side of the gate
+        protected int top; // top side of the gate
+        protected List<Pin> pins = new List<Pin>(); // pins list
+        protected bool selected = false; // selected state
 
 
-        /// <summary>
-        /// this will initialize a gate object
-        /// </summary>
-        /// <param name="x">represents the _x position of the gates</param>
-        /// <param name="y">this represents the y position of the gates</param>
+        // initialise an element object (public)
         public Elements(int x, int y)
         {
-            
+            MoveTo(x, y);
         }
 
 
-        /// <summary>
-        /// Gets the left hand edge of the gate
-        /// </summary>
-        public int Left
-        {
-            get { return left;  }
-        }
+        // localised vars
+        public int Left => left;
+        public int Top => top;
+        public List<Pin> Pins => pins;
 
-        /// <summary>
-        /// gets the top edge of the gates
-        /// </summary>
-        public int Top
-        { 
-            get { return top; }
-        }
 
-        /// <summary>
-        /// gets the list of pins for the gates
-        /// </summary>
-        public List<Pin> Pins
-        {
-            get { return pins; }
-        }
-
+        // selected bool
         public virtual bool Selected
         {
             get { return selected; }
@@ -69,28 +37,35 @@ namespace Circuits
         }
 
 
+        // W/H declaration, used in subclasses
+        protected abstract int Width { get; }
+        protected abstract int Height { get; }
 
-        /// <summary>
-        /// abstract method used for drawing other gates
-        /// </summary>
-        /// <param name="paper"></param>
+
+        // mouse hovering bool
+        public virtual bool IsMouseOn(int x, int y)
+        {
+            return left <= x && x < left + Width
+                && top <= y && y < top + Height;
+        }
+
+
+        // moving gate event
+        public virtual void MoveTo(int x, int y)
+        {
+            left = x;
+            top = y;
+        }
+
+
+        // abstract method used for drawing elements
         public abstract void Draw(Graphics paper);
 
-        /// <summary>
-        /// an abstract method used for evaluating circuit each gate will call on this method
-        /// with slight variations to represent the functions of difrent gates
-        /// </summary>
-        /// <returns></returns>
+        // an abstract method used for evaluating circuit each gate will call on this method
+        // with slight variations to represent the functions of differrent gates
         public abstract bool Evaluate();
 
-        /// <summary>
-        /// abstract method used to clone seperate gates
-        /// </summary>
-        /// <returns></returns>
+        // abstract method used to clone seperate gates
         public abstract Elements Clone();
-
-
-
-
     }
 }
